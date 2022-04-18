91f329e9b621933ffd77047747a39de75791fd77
Natalhaa <102674989+Natalhaa@users.noreply.github.com>
2022-04-18 15:40:51 -0300

    V2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let competidores= []
 function positions (firstPlace, secondPlace, lastPlace){
         if("Daniel" === lastPlace){
-        competidores.push(`1º Colocado: ${firstPlace} 2º Colocado: ${lastPlace} 3º Colocado: ${secondPlace}`)
+        competidores.push(`1º Colocado: ${firstPlace} 2º Colocado: ${lastPlace}
+         3º Colocado: ${secondPlace}`)
     }
+    else if ("Daniel" === secondPlace){
+        competidores.push(`1º Colocado: ${secondPlace} 2º Colocado: ${firstPlace}
+        3º Colocado: ${lastPlace}`)
+    }
+    else(
+        competidores.push(`1º Colocado: ${firstPlace} 2º Colocado: ${secondPlace}
+        3º Colocado: ${lastPlace}`)
+    )
     return competidores
 }
 console.log(positions("Rafael", "Manoel","Daniel"))
